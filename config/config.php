73ce6c56257d6501368bc0011655360e04615ef9
shukri0c <shukriamahdi05@gmail.com>
2025-07-24 19:38:49 +0100

    Added config so API key in one file

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'nasa_api' => [
+        'endpoint' => 'https://api.nasa.gov/planetary/apod',
+        'api_key' => 'tt2iSej6XA6MHceRa9RT379u0bYLhwQ9UAgAfe34' 
+    ],
+    'base_url' => 'http://localhost/NASAimages/view/view.php'
+]
